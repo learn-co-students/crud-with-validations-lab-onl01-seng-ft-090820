dfc96fb12ef0a439ff32a78cd2f304c316160391
HeartyRhombus <kaylahmg@gmail.com>
2020-11-07 14:24:28 -0800

    Done.

---
+++ app/validators/song_title_validator.rb
@@ -0,0 +1,10 @@
+class SongTitleValidator < ActiveModel::Validator
+    def validate
+        if Song.any? {|s| s.title == record.title &&
+            s.artist_name == record.artist_name &&
+            s.release_year == record.release_year}
+
+            record.errors[:title] << "cannot be repeated by the same artist in the same year"
+        end
+    end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Song.create(title: "Winter Wonderland", released: true, release_year: 2011, artist_name: "Michael Buble", genre: "Holiday")
+
+Song.create(title:"Rolling in the Deep", released: true, release_year: 2011, artist_name: "Adele", genre: "Pop")
