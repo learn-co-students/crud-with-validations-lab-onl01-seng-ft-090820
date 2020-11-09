8a383f6517220424e72eec5c5894629a57b87b0d
csdj92 <csdj92@gmail.com>
2020-11-09 14:47:18 -0500

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,6 @@
+class Song < ApplicationRecord
+    validates :title, presence: true, uniqueness: {scope: :release_year, message: "cannot be repeated in the same year"}
+    validates :released, inclusion: {in: [true, false]}
+    validates :artist_name, presence: true
+    validates :release_year, presence: true, if: :released?, numericality: { less_than: Time.now.year}
+end
